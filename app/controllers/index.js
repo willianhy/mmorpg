86b97a719a02c11a0163fe8bc993db5caf11fa3f
Willian Yassunaka <willianhy@gmail.com>
2018-07-04 23:05:57 -0300

    auth controller

--- app/controllers/index.js
+++ app/controllers/index.js
@@ -1,3 +1,18 @@
 module.exports.index = function(application, req, res){
     res.render('index');
 }
+
+module.exports.autenticar = function(application, req, res){
+    var dadosForm = req.body;
+    req.assert('usuario', 'Usuário não deve ser vazio').notEmpty();
+    req.assert('senha', 'Senha não deve ser vazia').notEmpty();
+
+    var erros = req.validationErrors();
+
+    if (erros){
+        res.render("index", {validacao: erros});
+        return;
+    }
+
+    res.send('tudo ok');
+}
