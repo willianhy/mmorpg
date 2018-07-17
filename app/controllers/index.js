62f401cbe1f2f9a02d451cbe7c09b0380568bf3d
Willian Yassunaka <willianhy@gmail.com>
2018-07-16 23:13:39 -0300

    session authentication

--- app/controllers/index.js
+++ app/controllers/index.js
@@ -14,5 +14,10 @@ module.exports.autenticar = function(application, req, res){
         return;
     }
 
-    res.send('tudo ok');
+    var connection = application.config.dbConnection;
+    var UsuariosDAO = new application.app.models.UsuariosDAO(connection);
+
+    UsuariosDAO.autenticar(dadosForm, req, res);
+
+    //res.send('tudo ok');
 }
