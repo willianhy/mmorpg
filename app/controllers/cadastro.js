de2e711e0d57a81d2865cfffe62e7c7375096d23
Willian Yassunaka <willianhy@gmail.com>
2018-07-17 22:31:42 -0300

    registering game

--- app/controllers/cadastro.js
+++ app/controllers/cadastro.js
@@ -19,7 +19,9 @@ module.exports.cadastrar = function(application, req, res){
 
     var connection = application.config.dbConnection;
     var UsuariosDAO = new application.app.models.UsuariosDAO(connection);
+    var JogoDAO = new application.app.models.JogoDAO(connection);
+    
     UsuariosDAO.inserirUsuario(dadosForm);
-
+    JogoDAO.gerarParametros(dadosForm.usuario);
     res.send('Podemos cadastrar');
 }
